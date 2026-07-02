9854a570f757ba78adf73b5ad69d139bd6938057
Amir M. M. Farid <amir.m.m.farid@gmail.com>
2026-07-02 10:18:33 -0400

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  site: "https://amirfarid.net",
   integrations: [mdx()],
   vite: {
     cacheDir: process.env.ASTRO_VITE_CACHE_DIR ?? ".astro/vite",
