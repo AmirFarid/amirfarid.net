6e146c1ad740dd29e21467183dac51cd5967c927
Amir M. M. Farid <amir.m.m.farid@gmail.com>
2026-07-05 00:40:18 -0400

    Strengthen identity SEO

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,11 +1,14 @@
 export const siteMetadata = {
   name: "Amir M. M. Farid",
+  alternateNames: ["AmirFarid", "Amir Farid", "Amir M. Farid"],
   title: "Amir M. M. Farid",
   description:
     "Personal academic website of Amir M. M. Farid, a Computer Science graduate student working on secure, private, and reliable decentralized systems.",
   url: "https://amirfarid.net",
   locale: "en_US",
   author: "Amir M. M. Farid",
+  affiliation: "Michigan Technological University",
+  role: "Computer Science graduate student",
   profileImage: "https://github.com/AmirFarid.png",
   sameAs: ["https://github.com/AmirFarid"],
 };
